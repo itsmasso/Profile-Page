6c7612c66dbc2de1f87df0017c727675759a1c58
Sammy <96604660+itsmasso@users.noreply.github.com>
2025-05-10 02:24:44 -0700

    bug fixes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import userRoutes from "./Routes/UserRoutes.js";
 import cookieParser from "cookie-parser";
+
+
 const PORT = process.env.PORT || 3001;
-dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173', 
-  credentials: true,
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://profile-page-git-main-itsmassos-projects.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use("/api/users", userRoutes); 
+app.use("/api/users", userRoutes);
 app.use("/uploads", express.static("uploads"));
 
 app.listen(PORT, () => {
